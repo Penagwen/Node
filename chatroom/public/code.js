a5b8561fc07f802ba7567886ac2cec59a71b32d7
Penagwen <83249342+Penagwen@users.noreply.github.com>
2022-11-25 20:12:49 -0600

    update

--- chatroom/public/code.js
+++ chatroom/public/code.js
@@ -13,6 +13,16 @@
         app.querySelector(".join-screen").classList.remove("active");
         app.querySelector(".chat-screen").classList.add("active");
     });
+    app.querySelector(".join-screen").addEventListener('keydown', function(event){
+        if(event.key == "Enter"){
+            let username  = app.querySelector(".join-screen #username").value;
+            if(username.length == 0){ return; }
+            socket.emit("newuser", username);
+            uname = username;
+            app.querySelector(".join-screen").classList.remove("active");
+            app.querySelector(".chat-screen").classList.add("active");
+        }
+    });
 
     app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
         let message = app.querySelector(".chat-screen #message-input").value;
@@ -26,6 +36,21 @@
             text: message
         });
         app.querySelector(".chat-screen #message-input").value = "";
+    });
+    app.querySelector(".chat-screen").addEventListener("keydown", function(event){
+        if(event.key == 'Enter'){
+            let message = app.querySelector(".chat-screen #message-input").value;
+            if(message.length == 0){ return; }
+            renderMessage("my", {
+                username: uname,
+                text: message
+            });
+            socket.emit("chat", {
+                username: uname,
+                text: message
+            });
+            app.querySelector(".chat-screen #message-input").value = "";
+        }
     });
 
     app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){
